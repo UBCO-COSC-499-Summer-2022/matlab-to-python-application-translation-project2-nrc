963cc3ae4897e56f3ad7b1b30e2f94fbba7c9350
Lucas Towers <luctowers@gmail.com>
2022-06-01 04:17:54 -0700

    Add stub alignment software prototype

---
+++ alignment-software/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from main_window import MainWindow
+
+def main(): 
+    root = tk.Tk()
+    app = MainWindow(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
