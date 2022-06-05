0d71cf40a8b0b70a64f2e2a5ea9e8802b4e00c22
josepena97 <penarevelo@gmail.com>
2022-06-05 12:39:58 -0700

    file rename and running modules as scripts

--- nrcemt/nanomi_optics/test/test_nanomi_optics.py
+++ nrcemt/nanomi_optics/test/test_nanomi_optics.py
@@ -2,4 +2,4 @@ from nrcemt.nanomi_optics.engine import nanomi_engine_greeting
 
 
 def test_main_message():
-    assert nanomi_engine_greeting() == "hello world from alignment engine!"
+    assert nanomi_engine_greeting() == "hello world from nanomi engine!"
