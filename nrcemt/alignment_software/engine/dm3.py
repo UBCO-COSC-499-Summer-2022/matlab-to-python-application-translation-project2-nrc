adc49ba6460aeeda150dfc7cfb5e65ef6cd0cd01
Lucas Towers <luctowers@gmail.com>
2022-06-10 16:49:35 -0700

    Refactor DM3DataType to use composed underlying types

--- nrcemt/alignment_software/engine/dm3.py
+++ nrcemt/alignment_software/engine/dm3.py
@@ -1,5 +1,5 @@
 """
-This module allows reading of DM3 files.
+This module allows reading and writing of DM3 files.
 It is based on the following DM3 Image Format description:
 https://imagej.nih.gov/ij/plugins/DM3Format.gj.html
 """
@@ -70,6 +70,13 @@ class DM3Image:
         self.is_little_endian = is_little_endian
         self.tag_group = tag_group
 
+    def write(self, file):
+        """A DM3 file can be writtend via this method."""
+        write_big_endian_long(file, self.version)
+        write_big_endian_long(file, 0)
+        write_big_endian_long(file, 1 if self.is_little_endian else 0)
+        self.tag_group.write(file)
+
     @classmethod
     def read(cls, file):
         """A DM3 file can be read via this class method."""
@@ -91,6 +98,20 @@ class DM3TagGroup:
         self.is_sorted = is_sorted
         self.is_open = is_open
         self.tags = []
+
+    def write(self, file):
+        write_byte(file, 1 if self.is_sorted else 0)
+        write_byte(file, 1 if self.is_open else 0)
+        write_big_endian_long(file, len(self.tags))
+        for entry_label, entry in self.tags:
+            if isinstance(entry, DM3TagGroup):
+                write_byte(file, TAG_GROUP_ENTRY)
+            elif isinstance(entry, DM3Data):
+                write_byte(file, TAG_DATA_ENTRY)
+            else:
+                raise ValueError("invalid DM3 tag entry type")
+            write_label(file, entry_label)
+            entry.write(file)
 
     @classmethod
     def read(cls, file):
@@ -135,12 +156,26 @@ class DM3Data:
     def decode(self):
         return self.data_type.decode(self.data_bytes)
 
+    def write(self, file):
+        if len(self.data_bytes) != self.data_type.size():
+            raise ValueError("DM3 data length does not match type")
+        file.write(b"%%%%")
+        type_length_pos = file.tell()
+        write_big_endian_long(file, 0)
+        self.data_type.write(file)
+        data_pos = file.tell()
+        type_length = data_pos - type_length_pos - 4
+        file.seek(type_length_pos)
+        write_big_endian_long(file, type_length)
+        file.seek(data_pos)
+        file.write(self.data_bytes)
+
     @classmethod
     def read(cls, file):
         if file.read(4) != b"%%%%":
             raise ValueError("invalid DM3 data encoding")
-        type_encodiing_length = read_big_endian_long(file)
-        if type_encodiing_length < 1:
+        type_length = read_big_endian_long(file)
+        if type_length < 1:
             raise ValueError("DM3 encoded type length must be >1")
         data_type = DM3DataType.read(file)
         data_bytes = file.read(data_type.size())
@@ -153,73 +188,118 @@ class DM3DataType:
     Can be a simple type or a complex nested type.
     """
 
-    def __init__(
-            self, encoded_type, length=None,
-            array_type=None, struct_fields=None):
+    def __init__(self, encoded_type, underlying_type):
         self.encoded_type = encoded_type
-        self.length = length
-        self.array_type = array_type
-        self.struct_fields = struct_fields
+        self.underlying_type = underlying_type
 
     def size(self):
-        if self.encoded_type in SCALAR_STRUCT:
-            return SCALAR_STRUCT[self.encoded_type].size
-        elif self.encoded_type == ENCODED_TYPES["STRUCT"]:
-            size = self.length
-            for field_name_length, field_type in self.struct_fields:
-                size += field_name_length + field_type.size()
-            return size
-        elif self.encoded_type == ENCODED_TYPES["STRING"]:
-            return 2 * self.length
-        elif self.encoded_type == ENCODED_TYPES["ARRAY"]:
-            return self.length * self.array_type.size()
-        else:
-            raise ValueError("unrecognized DM3 encoded type")
+        return self.underlying_type.size()
 
     def decode(self, data_bytes):
-        if self.encoded_type in SCALAR_STRUCT:
-            return SCALAR_STRUCT[self.encoded_type].unpack(data_bytes)[0]
-        elif self.encoded_type == ENCODED_TYPES["ARRAY"]:
-            if self.array_type.encoded_type in SCALAR_STRUCT:
-                return np.frombuffer(
-                    data_bytes,
-                    SCALAR_NUMPY_TYPES[self.array_type.encoded_type]
-                )
-            else:
-                raise NotImplementedError("non-scalar array decoding")
-        elif self.encoded_type == ENCODED_TYPES["STRING"]:
-            return data_bytes.decode("utf-16-le")
-        else:
-            return None
+        return self.underlying_type.decode(data_bytes)
+
 
     @classmethod
     def read(cls, file):
         encoded_type = read_big_endian_long(file)
         if encoded_type in SCALAR_STRUCT:
-            return cls(encoded_type)
+            return cls(encoded_type, DM3ScalarType(encoded_type))
         elif encoded_type == ENCODED_TYPES["STRUCT"]:
-            struct_name_length = read_big_endian_long(file)
-            struct_field_count = read_big_endian_long(file)
-            struct_fields = []
-            for _ in range(struct_field_count):
-                field_name_length = read_big_endian_long(file)
-                field_type = DM3DataType.read(file)
-                struct_fields.append((field_name_length, field_type))
-            return cls(
-                encoded_type,
-                length=struct_name_length,
-                struct_fields=struct_fields
-            )
+            return cls(encoded_type, DM3StructType.read(file))
         elif encoded_type == ENCODED_TYPES["STRING"]:
-            string_length = read_big_endian_long(file)
-            return cls(encoded_type, length=string_length)
+            return cls(encoded_type, DM3String.read(file))
         elif encoded_type == ENCODED_TYPES["ARRAY"]:
-            array_type = DM3DataType.read(file)
-            array_length = read_big_endian_long(file)
-            return cls(
-                encoded_type, length=array_length, array_type=array_type)
+            return cls(encoded_type, DM3ArrayType.read(file))
         else:
             raise ValueError(f"unrecognized DM3 encoded type {encoded_type}")
+
+
+class DM3ScalarType:
+
+    def __init__(self, encoded_type):
+        self.structure = SCALAR_STRUCT[encoded_type]
+
+    def decode(self, data_bytes):
+        return self.structure.unpack(data_bytes)[0]
+
+    def size(self):
+        return self.structure.size
+
+
+class DM3StructType:
+
+    def __init__(self, struct_name_length, field_name_lengths, field_types):
+        self.struct_name_length = struct_name_length
+        self.field_name_lengths = field_name_lengths
+        self.field_types = field_types
+
+    def decode(self, data_bytes):
+        raise NotImplementedError("dm3 struct decoding not implemented")
+
+    def size(self):
+        size = self.struct_name_length
+        for field_name_length in self.field_name_lengths:
+            size += field_name_length
+        for field_type in self.field_types:
+            size += field_type.size()
+        return size
+
+    @classmethod
+    def read(cls, file):
+        struct_name_length = read_big_endian_long(file)
+        struct_field_count = read_big_endian_long(file)
+        field_name_lengths = []
+        field_types = []
+        for _ in range(struct_field_count):
+            field_name_lengths.append(read_big_endian_long(file))
+            field_types.append(DM3DataType.read(file))
+        return cls(
+            struct_name_length,
+            field_name_lengths,
+            field_types
+        )
+
+
+class DM3String:
+
+    def __init__(self, string_length):
+        self.string_length = string_length
+
+    def decode(self, data_bytes):
+        return data_bytes.decode("utf-16-le")
+
+    def size(self):
+        return 2 * self.string_length
+
+    @classmethod
+    def read(cls, file):
+        string_length = read_big_endian_long(file)
+        return cls(string_length)
+
+
+class DM3ArrayType:
+
+    def __init__(self, array_type, array_length):
+        self.array_type = array_type
+        self.array_length = array_length
+
+    def decode(self, data_bytes):
+        if self.array_type.encoded_type in SCALAR_STRUCT:
+            return np.frombuffer(
+                data_bytes,
+                SCALAR_NUMPY_TYPES[self.array_type.encoded_type]
+            )
+        else:
+            raise NotImplementedError("non-scalar array decoding")
+
+    def size(self):
+        return self.array_length * self.array_type.size()
+
+    @classmethod
+    def read(cls, file):
+        array_type = DM3DataType.read(file)
+        array_length = read_big_endian_long(file)
+        return cls(array_type, array_length)
 
 
 def read_byte(file):
@@ -227,14 +307,29 @@ def read_byte(file):
     return Struct("B").unpack(file.read(1))[0]
 
 
+def write_byte(file, n):
+    """Writes a single byte from a file."""
+    file.write(Struct("B").pack(n))
+
+
 def read_big_endian_short(file):
     """Reads a two byte BE short from a file."""
     return Struct(">H").unpack(file.read(2))[0]
 
 
+def write_big_endian_short(file, n):
+    """Writes a two byte BE short from a file."""
+    file.write(Struct(">H").pack(n))
+
+
 def read_big_endian_long(file):
     """Reads a four byte BE long from a file."""
     return Struct(">I").unpack(file.read(4))[0]
+
+
+def write_big_endian_long(file, n):
+    """Writes a four byte BE long from a file."""
+    file.write(Struct(">I").pack(n))
 
 
 def read_label(file):
@@ -244,3 +339,12 @@ def read_label(file):
     """
     length = read_big_endian_short(file)
     return file.read(length).decode("latin-1")
+
+
+def write_label(file, string):
+    """
+    Reads a 1 character per byte string from a file.
+    encoding: https://en.wikipedia.org/wiki/ISO/IEC_8859-1
+    """
+    write_big_endian_short(file, len(string))
+    file.write(string.encode("latin-1"))
