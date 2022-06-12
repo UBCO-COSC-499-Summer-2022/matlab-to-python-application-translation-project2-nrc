86f8b2c174a772d7b8302c38079123b7e554be55
Lucas Towers <luctowers@gmail.com>
2022-06-12 14:05:23 -0700

    Add second to test for outlier rejection

--- nrcemt/alignment_software/test/test_img_processing.py
+++ nrcemt/alignment_software/test/test_img_processing.py
@@ -16,7 +16,7 @@ def test_convert_img_float64():
     assert np.array_equal(img_float64, img_expected)
 
 
-def test_reject_outliers_percentile():
+def test_reject_outliers_percentile_normal():
     img = np.random.normal(0.4, 0.1, 20000).reshape((200, 100))
     img_copy = np.copy(img)
     minimum, maximum = reject_outliers_percentile(img, 2.0)
@@ -26,3 +26,16 @@ def test_reject_outliers_percentile():
     assert minimum < maximum
     assert (img < minimum).sum() == 200
     assert (img > maximum).sum() == 200
+
+
+def test_reject_outliers_percentile_random():
+    img = np.random.uniform(0.0, 1.0, 10000).reshape((100, 100))
+    img_copy = np.copy(img)
+    minimum, maximum = reject_outliers_percentile(img, 1.0)
+    # make sure the original image was not modified
+    assert np.array_equal(img, img_copy)
+    # check whether minimum and maximum reject the correct number of elements
+    assert minimum < maximum
+    assert (img < minimum).sum() == 50
+    assert (img > maximum).sum() == 50
+
