ae2cc672bd79eccd3ec92564ee835ab3011d51fd
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-12 16:51:56 -0700

    ran autopep8

--- nrcemt/qeels/gui/main.py
+++ nrcemt/qeels/gui/main.py
@@ -6,68 +6,69 @@ from plasmonSection import *
 
 
 def main():
-    size=25
+    size = 25
     root = tk.Tk()
     # set title
     root.title("qEEls peak detection")
-    
+
     # setting size of window(will change later)
-    root.geometry("1500x700+10+0")   
+    root.geometry("1500x700+10+0")
 
-    #THESE MIGHT NEED TO BE PUT IN A GRID
-    inputs=ttk.Frame(root)
-    #Bulk Plasmons  
-    bulk_plasmon1=plasmonSelect(inputs,"Bulk Plasmon 1",size)
-    bulk_plasmon2=plasmonSelect(inputs,"Bulk Plasmon 2",size)
-    bulk_plasmon1.grid(row=0,column=0,padx=10,pady=10)
-    bulk_plasmon2.grid(row=0,column=1,padx=10,pady=10)
-    
-    #Surface Plasmon Upper
-    upper_plasmon1=plasmonSelect(inputs,"Surface Plasmon Upper 1",size)
-    upper_plasmon2=plasmonSelect(inputs,"Surface Plasmon Upper 2",size)
-    upper_plasmon1.grid(row=1,column=0,padx=10,pady=10)
-    upper_plasmon2.grid(row=1,column=1,padx=10,pady=10)
+    # THESE MIGHT NEED TO BE PUT IN A GRID
+    inputs = ttk.Frame(root)
+    # Bulk Plasmons
+    bulk_plasmon1 = plasmonSelect(inputs, "Bulk Plasmon 1", size)
+    bulk_plasmon2 = plasmonSelect(inputs, "Bulk Plasmon 2", size)
+    bulk_plasmon1.grid(row=0, column=0, padx=10, pady=10)
+    bulk_plasmon2.grid(row=0, column=1, padx=10, pady=10)
 
-    #Surface Plasmon Lower
-    lower_plasmon1=plasmonSelect(inputs,"Surface Plasmon Lower 1",size)
-    lower_plasmon2=plasmonSelect(inputs,"Surface Plasmon Lower 2",size)
-    lower_plasmon1.grid(row=2,column=0,padx=10,pady=10)
-    lower_plasmon2.grid(row=2,column=1,padx=10,pady=10)
+    # Surface Plasmon Upper
+    upper_plasmon1 = plasmonSelect(inputs, "Surface Plasmon Upper 1", size)
+    upper_plasmon2 = plasmonSelect(inputs, "Surface Plasmon Upper 2", size)
+    upper_plasmon1.grid(row=1, column=0, padx=10, pady=10)
+    upper_plasmon2.grid(row=1, column=1, padx=10, pady=10)
+
+    # Surface Plasmon Lower
+    lower_plasmon1 = plasmonSelect(inputs, "Surface Plasmon Lower 1", size)
+    lower_plasmon2 = plasmonSelect(inputs, "Surface Plasmon Lower 2", size)
+    lower_plasmon1.grid(row=2, column=0, padx=10, pady=10)
+    lower_plasmon2.grid(row=2, column=1, padx=10, pady=10)
     inputs.pack(anchor=tk.NW)
-    
-    #Average Pixel
-    results=ttk.Frame(root)
-    average_pixel=result_boxes(results,"Average Pixel",size)
-    average_pixel.pack()
-    #Micro rad/pixel upper
-    rad_upper=result_boxes(results,"Micro rad/Pixel Upper",size)
-    rad_upper.pack()
-    #Micro rad/pixel lower
-    rad_lower=result_boxes(results,"Micro rad/Pixel Lower",size)
-    rad_lower.pack()
-    #Ev/Pixel
-    ev=result_boxes(results,"EV/Pixel",size)
-    ev.pack()
-    results.pack(anchor=tk.W,pady=60,padx=10)
 
-    #adding buttons
-    button_frame=ttk.Frame(root)
-    ttk.Style(button_frame).configure("TButton",font=size)
-    open_button=ttk.Button(button_frame,text="Open Image")
-    detect_button=ttk.Button(button_frame,text="Detect")
-    save_button=ttk.Button(button_frame,text="Save Data")
-    reset_button=ttk.Button(button_frame,text="Reset")
-    open_button.pack(side="left",padx=10,pady=10)
-    detect_button.pack(side="left",padx=10,pady=10)
-    save_button.pack(side="left",padx=10,pady=10)
-    reset_button.pack(side="left",padx=10,pady=10)
-    button_frame.pack(side="left",anchor=tk.W)
-    
-    #Dislpaying a temp image to help adjust UI
-    photo=tk.PhotoImage(file="./nrcemt/qeels/spect.png",height=600)
-    ttk.Label(root,image=photo,compound="top").place(x=700,y=10)
-    
+    # Average Pixel
+    results = ttk.Frame(root)
+    average_pixel = result_boxes(results, "Average Pixel", size)
+    average_pixel.pack()
+    # Micro rad/pixel upper
+    rad_upper = result_boxes(results, "Micro rad/Pixel Upper", size)
+    rad_upper.pack()
+    # Micro rad/pixel lower
+    rad_lower = result_boxes(results, "Micro rad/Pixel Lower", size)
+    rad_lower.pack()
+    # Ev/Pixel
+    ev = result_boxes(results, "EV/Pixel", size)
+    ev.pack()
+    results.pack(anchor=tk.W, pady=60, padx=10)
+
+    # adding buttons
+    button_frame = ttk.Frame(root)
+    ttk.Style(button_frame).configure("TButton", font=size)
+    open_button = ttk.Button(button_frame, text="Open Image")
+    detect_button = ttk.Button(button_frame, text="Detect")
+    save_button = ttk.Button(button_frame, text="Save Data")
+    reset_button = ttk.Button(button_frame, text="Reset")
+    open_button.pack(side="left", padx=10, pady=10)
+    detect_button.pack(side="left", padx=10, pady=10)
+    save_button.pack(side="left", padx=10, pady=10)
+    reset_button.pack(side="left", padx=10, pady=10)
+    button_frame.pack(side="left", anchor=tk.W)
+
+    # Dislpaying a temp image to help adjust UI
+    photo = tk.PhotoImage(file="./nrcemt/qeels/spect.png", height=600)
+    ttk.Label(root, image=photo, compound="top").place(x=700, y=10)
+
     root.mainloop()
- 
+
+
 if __name__ == "__main__":
     main()
--- nrcemt/qeels/gui/plasmonSection.py
+++ nrcemt/qeels/gui/plasmonSection.py
@@ -2,67 +2,49 @@ import tkinter as tk
 from tkinter import ttk
 from nrcemt.qeels.engine import qeels_engine_greeting
 
+
 class plasmonSelect(ttk.Frame):
     # initializing
-    def __init__(self,master,name,fsize):
-        #font size
-        self.fsize=fsize
-        
-        #padding
-        self.xpad=5
-        self.ypad=5
+    def __init__(self, master, name, fsize):
+        # font size
+        self.fsize = fsize
+
+        # padding
+        self.xpad = 5
+        self.ypad = 5
         super().__init__(master)
-        self.name=name
-        #new frame
-        frm=ttk.Frame(self)
-        
-        #creates styling for radio buttons
-        ttk.Style(self).configure("TRadiobutton",font=self.fsize)
+        self.name = name
+        # new frame
+        frm = ttk.Frame(self)
+
+        # creates styling for radio buttons
+        ttk.Style(self).configure("TRadiobutton", font=self.fsize)
         # creates the radio button
-        self.radio_btn = ttk.Radiobutton(self,text=name,width=25)
+        self.radio_btn = ttk.Radiobutton(self, text=name, width=25)
         self.radio_btn.pack(anchor=tk.W)
 
-        #creates label
-        self.lbl = ttk.Label(frm,text="X: ",font=self.fsize)
-        self.lbl.pack(side="left",padx=self.xpad,pady=self.ypad)
-        
-        #Creates entry box
-        self.x_entry=ttk.Entry(frm, width=7)
-        self.x_entry.pack(side="left",padx=self.xpad,pady=self.ypad)
-        
-        #creates label
-        self.lbl = ttk.Label(frm,text="Y: ",font=self.fsize)
-        self.lbl.pack(side="left",padx=self.xpad,pady=self.ypad)
-        
-        #Creates entry box
-        self.x_entry=ttk.Entry(frm, width=7)
-        self.x_entry.pack(side="left",padx=self.xpad,pady=self.ypad)
+        # creates label
+        self.lbl = ttk.Label(frm, text="X: ", font=self.fsize)
+        self.lbl.pack(side="left", padx=self.xpad, pady=self.ypad)
+
+        # Creates entry box
+        self.x_entry = ttk.Entry(frm, width=7)
+        self.x_entry.pack(side="left", padx=self.xpad, pady=self.ypad)
+
+        # creates label
+        self.lbl = ttk.Label(frm, text="Y: ", font=self.fsize)
+        self.lbl.pack(side="left", padx=self.xpad, pady=self.ypad)
+
+        # Creates entry box
+        self.x_entry = ttk.Entry(frm, width=7)
+        self.x_entry.pack(side="left", padx=self.xpad, pady=self.ypad)
         frm.pack()
-        
-class rows(ttk.Frame):
-    def __init__(self,master,name1,name2,radio_var,x1_var,y1_var,x2_var,y2_var,width,detect):
-        super().__init__(master)
-        #creates all asociated buttons/entrys for 1st plasmon locations
-        plasmon_1=plasmonSelect(self,name1,radio_var,x1_var,y1_var)
-        plasmon_1.pack(side="left",anchor=tk.NW)
-        
-        #creates all asociated buttons/entrys for 2nd plasmon locations
-        plasmon_2=plasmonSelect(self,name2,radio_var,x2_var,y2_var)
-        plasmon_2.pack(side="left",anchor=tk.NW)
-        
-        #Width
-        ttk.Label(self,text="Width: ").pack(side="left",anchor=tk.W)  
-        
-        width_entry=ttk.Entry(self,textvariable=width,width=7)
-        width_entry.pack(side="left")      
-        
-        detect_box=ttk.Checkbutton(self,variable=detect,text="Detect")
-        detect_box.pack(side="left")
+
 
 class result_boxes(ttk.Frame):
-    def __init__(self,master,name,fsize):
+    def __init__(self, master, name, fsize):
         super().__init__(master)
-        ev_label=ttk.Label(self,text=name + ": ",font=fsize,width=20)
-        ev_entry=ttk.Entry(self,width=7)
+        ev_label = ttk.Label(self, text=name + ": ", font=fsize, width=20)
+        ev_entry = ttk.Entry(self, width=7)
         ev_label.pack(side="left")
         ev_entry.pack(side="left")
