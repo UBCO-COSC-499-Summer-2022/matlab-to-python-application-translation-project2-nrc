2498378773b9d21fc57e32597d17e04a1353c234
Lucas Towers <luctowers@gmail.com>
2022-06-12 15:56:43 -0700

    Add doc string to image processing

--- nrcemt/alignment_software/engine/img_processing.py
+++ nrcemt/alignment_software/engine/img_processing.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def convert_img_float64(img):
+    """Converts an image matrix to float64 type on the range [0.0, 1.0]."""
     float64_img = img.astype(np.float64)
     minimum = float64_img.min()
     maximum = float64_img.max()
@@ -11,14 +12,19 @@ def convert_img_float64(img):
     return float64_img
 
 
-def reject_outliers_percentile(img, percentile):
+def reject_outliers_percentile(img, percent):
+    """
+    Returns a minimum and maxium threshold which elminate a given percent of
+    percent of outliers.
+    """
     img_sorted = np.sort(img.ravel())
-    rejection_count = len(img_sorted) * percentile / 200
+    rejection_count = len(img_sorted) * percent / 200
     minimum_index = math.floor(rejection_count)
     maximum_index = math.ceil(len(img_sorted) - 1 - rejection_count)
     return (img_sorted[minimum_index], img_sorted[maximum_index])
 
 
 def adjust_img_range(img, min1, max1, min2, max2):
+    """Maps image values from a range [min1, max1] to another [min2, max2]."""
     img_normalized = (img - min1) / (max1 - min1)
     return img_normalized * (max2 - min2) + min2
