1ca8cab3e89dbafd9c736f9afdf737c1baf0381b
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-06-12 22:43:34 -0700

    add slider layout
    MAJOR layout issues rn with the upper widgets but things are happening

--- nrcemt/nanomi_optics/gui/widget_config.py
+++ nrcemt/nanomi_optics/gui/widget_config.py
@@ -18,6 +18,16 @@ class UpperWidgets(ttk.Frame):
         self.option_var = tk.StringVar(self)
         self.create_widget
         
+        # label for sliders
+        self.sliders_label = ttk.Label(self.upper_frame, text="Lens settings (nm):")
+        self.sliders_label.pack(side="left", padx=X_PADDING, pady=Y_PADDING)
+        
+        #call 3 sliders with label names 
+        SliderLayout.layout("C1: ")
+        SliderLayout.layout("C2: ")
+        SliderLayout.layout("C3: ")
+        
+        
     def create_widget(self):
         #label for drop down box
         mode_label = ttk.Label(self.upper_frame, text="Mode: ")
@@ -31,7 +41,37 @@ class UpperWidgets(ttk.Frame):
         option_menu.pack(side="left", padx=X_PADDING, pady=Y_PADDING)
 
 
-# make class that makes a standard slider
+# make class that makes a standard slider layout with: label, slider, box, and toggle
+class SliderLayout(ttk.Frame):
+    
+    def layout(name):
+        
+        # new frame that contains widgets
+        slider_frame = ttk.Frame()
+        
+        # creates label
+        x_label = ttk.Label(slider_frame, text=name)
+        x_label.pack(side="left", padx=X_PADDING, pady=Y_PADDING)
+        
+        # creates slider
+        slider = ttk.Scale(slider_frame, from_=0, to = 100, orient='horizontal',)
+        
+        # creates entry box
+        entry = ttk.Entry(slider_frame, width=9)
+        entry.pack(side="left", padx=X_PADDING, pady=Y_PADDING)
+        
+        #make toggle
+        #toggle function
+        def Toggle():
+            if toggle.config("text")[-1] == "ON":
+                toggle.config(text="OFF")
+            else:
+                toggle.config(text="ON")
+        # creates on/of toggle button
+        toggle = ttk.Button(text="OFF", width=10, command=Toggle)
+        toggle.pack(side="left", padx=X_PADDING, pady=Y_PADDING)
+    
+        
 
 # widgets for lower settings adjustment
 class LowerWidgets(ttk.Frame):
@@ -51,6 +91,4 @@ class DisplaySettings(ttk.Frame):
         # new frame that contains the display of settings and results to the user
         self.display_frame = ttk.Frame(self) 
 
-# make class that makes a standard radio button
-
-
+# class that makes a standard radio button layout
