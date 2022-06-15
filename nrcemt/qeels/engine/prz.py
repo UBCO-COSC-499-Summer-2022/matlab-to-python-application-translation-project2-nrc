ffd126069fcb9873e92d2d6cbacbffa9439b81e0
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-14 22:28:43 -0700

    adressed comments

--- nrcemt/qeels/engine/prz.py
+++ nrcemt/qeels/engine/prz.py
@@ -1,11 +1,6 @@
 import numpy as np
-import matplotlib as plt
-import matplotlib.pyplot as plt
 
 
-# SHOULD I RENAME TO read() SIMILAR TO IN dm3.py
-
-
-def read_prz(file_path):
-    spectrogram=np.load(file_path, allow_pickle=True)
+def load_prz(file_path):
+    spectrogram = np.load(file_path, allow_pickle=True)
     return spectrogram
