aa848b13f8614752d6d076b6e46c43d6cd3e97d6
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-14 20:07:28 -0700

    setting up branch

---
+++ nrcemt/qeels/engine/read_prz.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class prz:
+
+    def __init__(self):
+        self.file_path = "C:\Users\garre\Desktop\Capstone project-20220530T173400Z-001\Capstone project\qEELS\1_qEELS_1deg_sum.prz"
+    
+    # SHOULD I RENAME TO read() SIMILAR TO IN dm3.py
+    def read_prz(file):
+        print(file)
--- nrcemt/qeels/test/test_queels.py
+++ nrcemt/qeels/test/test_queels.py
@@ -3,3 +3,6 @@ from nrcemt.qeels.engine import qeels_engine_greeting
 
 def test_main_message():
     assert qeels_engine_greeting() == "hello world from qeels engine!"
+
+def test_read_prz():
+    pass
