9660061497a2a0f434c2f499534dfdae90a5128b
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-14 22:11:56 -0700

    added prz reading

---
+++ nrcemt/qeels/engine/main.py
@@ -0,0 +1,9 @@
+from read_prz import read_prz
+
+
+def main():
+    read_prz("sadajbdfkjsnkd")
+
+
+if __name__ == "__main__":
+    main()
--- nrcemt/qeels/engine/read_prz.py
+++ nrcemt/qeels/engine/read_prz.py
@@ -1,11 +1,14 @@
 import numpy as np
+import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
-class prz:
+# SHOULD I RENAME TO read() SIMILAR TO IN dm3.py
 
-    def __init__(self):
-        self.file_path = "C:\Users\garre\Desktop\Capstone project-20220530T173400Z-001\Capstone project\qEELS\1_qEELS_1deg_sum.prz"
-    
-    # SHOULD I RENAME TO read() SIMILAR TO IN dm3.py
-    def read_prz(file):
-        print(file)
+
+def read_prz(file):
+    spectrogram=np.load("C:\\Users\\garre\\Desktop\\Capstone project-20220530T173400Z-001\\Capstone project\\qEELS\\1_qEELS_1deg_sum.prz", allow_pickle=True)
+    plt.axes([0.25, 0.25, 0.6, 0.6]).set_axis_off()
+    plt.imshow(np.log(spectrogram['data']+1))
+    plt.show()
+    return spectrogram
