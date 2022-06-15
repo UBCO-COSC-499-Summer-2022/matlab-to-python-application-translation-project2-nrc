56cd1c74f4f1f6c05f8a04c99266498119e85cc5
Lucas Towers <luctowers@gmail.com>
2022-06-14 21:36:39 -0700

    Remove hello world tests

--- nrcemt/nanomi_optics/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from nrcemt.common import common_greeting
-
-
-def nanomi_engine_greeting():
-    return common_greeting() + " from nanomi engine!"
--- nrcemt/qeels/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from nrcemt.common import common_greeting
-
-
-def qeels_engine_greeting():
-    return common_greeting() + " from qeels engine!"
