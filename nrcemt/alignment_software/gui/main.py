da1f03bc890d2c5738eb0288ae226fab68b8b1d0
josepena97 <penarevelo@gmail.com>
2022-06-15 11:15:17 -0700

    Restructure alignment main window

--- nrcemt/alignment_software/gui/main.py
+++ nrcemt/alignment_software/gui/main.py
@@ -1,4 +1,4 @@
-from nrcemt.alignment_software.gui.windows.main_window import MainWindow
+from .main_window import MainWindow
 
 
 def main():
--- nrcemt/alignment_software/gui/main_window.py
+++ nrcemt/alignment_software/gui/main_window.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from nrcemt.alignment_software.gui.frames.tool_frame import ToolFrame
-from nrcemt.alignment_software.gui.frames.image_frame import ImageFrame
+from .frames.tool_frame import ToolFrame
+from .frames.image_frame import ImageFrame
 
 
 class MainWindow(tk.Tk):
@@ -8,10 +8,7 @@ class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry("800x600")
-
-        # Setting up grid
-        # self.columnconfigure(0, weight=1)
-        # self.columnconfigure(1, weight=4)
+        self.title("Alignment Main Window")
 
         # Adding widgets to the window
         self.tools = ToolFrame(self)
