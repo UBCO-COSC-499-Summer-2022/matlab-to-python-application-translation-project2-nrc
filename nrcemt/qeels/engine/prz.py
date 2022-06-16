0acd700ec354f70f79237e74838883b79edb79a6
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-15 17:27:11 -0700

    Image displayed(Not in gui)

--- nrcemt/qeels/engine/prz.py
+++ nrcemt/qeels/engine/prz.py
@@ -1,10 +1,14 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def load_prz(file_path):
     spectrogram = np.load(file_path, allow_pickle=True)
+    render_prz(spectrogram['data'])
     return spectrogram['data']
 
+
 def render_prz(spectrogram):
-    # Temp
-    pass
+    # temporary solution for displaying image
+    plt.axes([0.25, 0.25, 0.6, 0.6]).set_axis_off()
+    plt.imshow(np.log(spectrogram+1))
+    plt.show()
