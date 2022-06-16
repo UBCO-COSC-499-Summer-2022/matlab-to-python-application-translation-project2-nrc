33ed31fa2d0d35eab6da87451e0028409e2c59a6
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-06-15 17:55:27 -0700

    performed slight image processing

--- nrcemt/qeels/engine/prz.py
+++ nrcemt/qeels/engine/prz.py
@@ -8,7 +8,11 @@ def load_prz(file_path):
 
 
 def render_prz(spectrogram):
-    # temporary solution for displaying image
-    plt.axes([0.25, 0.25, 0.6, 0.6]).set_axis_off()
+    # Temporary solution for displaying image(mainly to perfect adjustments)
+    # plt.axes([0.25, 0.25, 0.6, 0.6]).set_axis_off()
+    min_intensity=np.amin(spectrogram)
+    max_intensity=np.amax(spectrogram)
+    print("min: " + str(min_intensity))
+    print("max: " + str(max_intensity))
     plt.imshow(np.log(spectrogram+1))
     plt.show()
