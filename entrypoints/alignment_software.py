e80e0b2cd2ebc6e601150503077da6ae5b5005e9
Lucas Towers <luctowers@gmail.com>
2022-06-16 18:53:03 -0700

    Add python entrypoints outside of nrcemt module

---
+++ entrypoints/alignment_software.py
@@ -0,0 +1,4 @@
+from nrcemt.alignment_software.gui.main import main
+
+if __name__ == "__main__":
+  main()
---
+++ entrypoints/qeels.py
@@ -0,0 +1,4 @@
+from nrcemt.qeels.gui.main import main
+
+if __name__ == "__main__":
+  main()
