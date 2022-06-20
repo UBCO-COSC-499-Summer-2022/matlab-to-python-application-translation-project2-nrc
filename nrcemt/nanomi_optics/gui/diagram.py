fe78e149c159660a2abc0254198e08f68547f57e
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-06-19 17:38:59 -0700

    better

--- nrcemt/nanomi_optics/gui/diagram.py
+++ nrcemt/nanomi_optics/gui/diagram.py
@@ -3,44 +3,48 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 
-# frame that holds the diagram
+# frame that holds the diagram (current values are placeholders)
 class ResultsLayout(ttk.Frame):
 
     def __init__(self, master):
         super().__init__(master, borderwidth=5)
 
-    # draws a box on x-axis
-    def Box(x, w, h, col, text, ax):
-        # x = location of centre point of box along x-axis
-        # w = width, h = height
-        # col = color
+        # draws a box on x-axis
+        def C1Box(x, w, h, col, text, ax):
+            # x = location of centre point of box along x-axis
+            # w = width, h = height, col = color
 
-        # smaller diameter lens bore (ground outer electrodes)
-        Lbore = 25.4*0.1/2
+            # smaller diameter lens bore (ground outer electrodes)
+            lens_bore = 25.4*0.1/2
 
-        # rectangle box
-        ax.add_patch(Rectangle((x-w/2, -h), w, h*2, edgecolor=col, facecolor='none', lw=1))
-        # top lens bore
-        ax.hlines(Lbore, x-w/2, x+w/2, colors=col)
-        # bottom lens bore
-        ax.hlines(-Lbore, x-w/2, x+w/2, colors=col)
-        # electrode location in lens
-        ax.vlines(x, -h, h, colors=col, linestyles='--')
+            # rectangle box
+            ax.add_patch(Rectangle(
+                (x-w/2, -h), w, h*2, edgecolor=col,
+                facecolor='none', lw=1))
+            # top lens bore
+            ax.hlines(lens_bore, x-w/2, x+w/2, colors=col)
+            # bottom lens bore
+            ax.hlines(-lens_bore, x-w/2, x+w/2, colors=col)
+            # electrode location in lens
+            ax.vlines(x, -h, h, colors=col, linestyles='--')
 
-        ax.text(x, -h-0.2, text, fontsize=8, rotation='horizontal', ha='center')
-        return
+            ax.text(x, -h-0.2, text, fontsize=8,
+                    rotation='horizontal', ha='center')
+            return
 
-    # plot
-    def diagramming():
+        # plot
+        def diagramming():
 
-        fig = plt.figure()
-        ax = fig.add_subplot()
-        fig.subplots_adjust(top=0.88, bottom=0.18)
+            fig = plt.figure()
+            ax = fig.add_subplot()
+            fig.subplots_adjust(top=0.88, bottom=0.18)
 
-        ax.axis([0, FX, -FY, FY])
-        ax.text(275, -2.1, 'Z [mm]', color = [0,0,0], fontsize=FS*1.2)
-        ax.set_ylabel('X [mm]', color = [0,0,0], fontsize=FS*1.2)
+            ax.axis([0, 600, -1.8, 1.8])
+            ax.text(275, -2.1, 'Z [mm]', color=[0, 0, 0],
+                    fontsize=8*1.2)
+            ax.set_ylabel('X [mm]', color=[0, 0, 0],
+                          fontsize=8*1.2)
 
-        # draw upper lenses
-        # draw C1 Lens
-        Box(C1_location, ThSymLens, PlH, ColSymLens,'C1', ax)
+            # draw upper lenses
+            # draw C1 Lens
+            C1Box(257, 63, 1.5, [0.3, 0.9, 0.65], 'C1', ax)
