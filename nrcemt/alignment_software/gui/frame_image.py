fb77039f12cad8a99b3593e1a8b920d1d7277912
Lucas Towers <luctowers@gmail.com>
2022-06-20 14:36:32 -0700

    Change frame image to use grid

--- nrcemt/alignment_software/gui/frame_image.py
+++ nrcemt/alignment_software/gui/frame_image.py
@@ -7,10 +7,12 @@ class ImageFrame(ttk.Frame):
 
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.figure = Figure(figsize=(8, 8), dpi=100)
         self.axis = self.figure.add_subplot()
         self.canvas = FigureCanvasTkAgg(self.figure, self)
-        self.canvas.get_tk_widget().pack()
+        self.canvas.get_tk_widget().grid(column=0, row=0, sticky="nwse")
 
     def render_image(self, img):
         self.axis.clear()
