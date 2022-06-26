a40ad6e638598a213a6c50c4ca735351a32bc89c
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-06-26 16:16:20 -0700

    C2 finish, C3 to sample

--- nrcemt/nanomi_optics/engine/lens_calculations.py
+++ nrcemt/nanomi_optics/engine/lens_calculations.py
@@ -34,4 +34,51 @@ def PlotCL3(UR, Cf, ray, fig, crossoverPoints, Cmag):
     y.append(rout_C1[0][0])
     y.append(rout2[0][0])
 
+    # effect of C2
+    rout_C2, zout_C2, d_C2, Mag2 = mlens(upper_lenses[1][0],
+                                      Cf[1], rout2, 0, 'C2',
+                                      crossoverPoints, Cmag)
+
+    # ray propagation in vacuum from C2 to Image 2 
+    rout_Im2,d_Im2 = mvac(upper_lenses[1][0], d_C2,rout_C2)
+    x.append(upper_lenses[1][0])
+    x.append(upper_lenses[1][0]+d_Im2)
+    y.append(rout_C2[0][0])
+    y.append(rout_Im2[0][0])
+
+    # ------- Image 2 to C3 to Image 3 -------
+    # ray propagation in vacuum from C2 to C3
+    rout3,d3 = mvac(upper_lenses[1][0],
+                    upper_lenses[2][0]-upper_lenses[1][0],
+                    rout_C2)
+    x.append(upper_lenses[1][0])
+    x.append(upper_lenses[1][0]+d3)
+    y.append(rout_C2[0][0])
+    y.append(rout3[0][0])
+
     return x, y
+
+    # effect of C3
+    rout_C3,zout_C3,d_C3,Mag3 = mlens(Czz[2],Cf[2],rout3,0,'C3', crossoverPoints, Cmag)
+
+
+    # ray propagation in vacuum from C3 to Image 3
+    rout_Im3,d_Im3 = mvac(Czz[2],d_C3,rout_C3)
+    x.append(Czz[2])
+    x.append(Czz[2]+d_Im3)
+    y.append(rout_C3[0][0])
+    y.append(rout_Im3[0][0])
+
+
+    # ------- C3 to sample plane ------
+    # ray propagation in vacuum from C2 to C3
+    rout_smpl,d_smpl = mvac(Czz[2],Sample_location-Czz[2],rout_C3)
+    x.append(Czz[2])
+    x.append(Czz[2]+d_smpl)
+    y.append(rout_C3[0][0])
+    y.append(rout_smpl[0][0])
+
+    # finding the beam farthest from the optics axis
+    global routMax
+    if abs(rout_smpl[0]) > abs(routMax[0]):
+        routMax = rout_smpl
