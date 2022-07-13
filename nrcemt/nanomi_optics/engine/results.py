2e298dff1c9d1d49520fd85c570a724fc8d69d83
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-07-13 16:49:52 -0700

    begin

---
+++ nrcemt/nanomi_optics/engine/results.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def save_results(file_path, headers, data):
+    with open(file_path, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        # write titles
+        csv_writer.writerow(headers)
+        # write results
+        csv_writer.writerows(data)
--- nrcemt/nanomi_optics/gui/frame_results.py
+++ nrcemt/nanomi_optics/gui/frame_results.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import ttk
 from .widget_templates import TableLayout
 
@@ -32,3 +33,23 @@ class ResultsFrame(ttk.LabelFrame):
         # magnification label
         magnification = ttk.Label(self, text="Magnification = 10")
         magnification.pack(side="top", anchor="nw", pady=PAD_Y)
+
+        # save data button
+        self.save_button = ttk.Button(
+            self, text="Save Data",
+        )
+        self.save_button.pack(
+            side="left",
+            pady=PAD_Y,
+            command=self.save_results
+        )
+
+    def save_results(self):
+
+        save_path = tk.filedialog.asksaveasfile(
+            mode='w',
+            defaultextension=".csv",
+            filetypes=[("CSV File", "*.csv")]
+        )
+        if save_path is not None:
+            save_path = save_path.name
