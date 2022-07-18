2b5359bf5ea99bcf377bd0d2a99bb7fbf8f613f8
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-07-18 14:15:16 -0600

    addressed other comment

--- nrcemt/qeels/engine/peak_detection.py
+++ nrcemt/qeels/engine/peak_detection.py
@@ -1,11 +1,5 @@
 import math
 import numpy as np
-from scipy import signal
-import scipy
-# Constants
-# PLANCK_CONSTANT = 4.1357*10 ^ (-15)
-# SPEED_LIGHT = 3*10 ^ (8)
-# Q_PIXEL = 0.001165934*10 ^ (9)
 
 
 def compute_rect_corners(x1, y1, x2, y2, width):
@@ -31,97 +25,6 @@ def compute_rect_corners(x1, y1, x2, y2, width):
     y_calculated = y2+width/2*math.cos(tilt_angle)
     res.append([round(x_calculated), round(y_calculated)])
     return res
-
-
-# def peak_detection(
-#     plasmon_array, width_array,
-#     results_array, detect_array,
-#     spectrogram
-# ):
-
-#     # retrieve average pixel, ev/pixel, microrad/pixel
-#     average_pixel = results_array[3]
-#     # e_dispersion = results_array[0]
-#     # q_dispersion_upper = results_array[1]
-
-#     # loop through different rows
-#     for i in range(0, 6, 2):
-#         # retrieve data for more use later on
-#         x1 = plasmon_array[i][0]
-#         y1 = plasmon_array[i][1]
-#         x2 = plasmon_array[i+1][0]
-#         y2 = plasmon_array[i+1][1]
-#         width = width_array[int(i/2)]
-#         detect = detect_array[int(i/2)]
-#         [spectrogram_width, spectrogram_height] = np.shape(spectrogram)
-
-#         # Needs better names
-#         is_filled_1 = x1 > 0 or y1 > 0
-#         is_filled_2 = x2 > 0 or y2 > 0
-
-#         # if both values are entered
-#         if detect and is_filled_1 and is_filled_2:
-#             # Peak finding tolerance
-#             tolerance = 1.01
-#             # Confidence number
-#             cn = 2
-
-#             # DOUBLE CHECK, BUT I DONT THINK THE RESULT FROM THIS ARE USED
-#             calculated_corners = compute_rect_corners(x1, y1, x2, y2, width)
-
-#             delta_x = x1-x2
-#             delta_y = y1-y2
-
-#             rotation_angle_rad = math.atan2(delta_y, delta_x)
-#             rotation_angle_degrees = math.degrees(rotation_angle_rad)
-
-#             rotation_angle_rad, rotation_angle_degrees = calc_angle(
-#                 x1, y1,
-#                 x2, y2
-#             )
-
-#             # apply rotation to the points
-#             x1, y1, x2, y2 = rotate_points(
-#                 x1, y1, x2, y2,
-#                 rotation_angle_rad,
-#                 spectrogram_width,
-#                 spectrogram_height
-#             )
-
-#             # rotate image so plasmon is vertica
-#             spectrogram_rotated = scipy.ndimage.rotate(
-#                 spectrogram,
-#                 rotation_angle_degrees*-1,
-#                 reshape=False
-#             )
-
-#             # Find absolute value of image
-#             spectrogram_signal = np.absolute(spectrogram_rotated)
-
-#             # Ensure y1 is less than y2
-#             if y1 > y2:
-#                 temp = y1
-#                 y1 = y2
-#                 y2 = temp
-
-#             # loops through rows of box
-#             print(np.sum(spectrogram_signal), cn, calculated_corners)
-#             for j in range(int(y1), int(y2)):
-#                 # mean of the row?
-#                 spectrogram_ycfit = ycfit(
-#                     spectrogram_signal,
-#                     average_pixel,
-#                     j, width, x1,
-#                     np.sum(spectrogram_signal)
-#                 )
-
-#                 index, _ = signal.find_peaks(
-#                     spectrogram_ycfit,
-#                     threshold=tolerance
-#                 )
-
-#         else:
-#             pass
 
 
 def ycfit(signal, average_pixel, it, width, x1, sum):
