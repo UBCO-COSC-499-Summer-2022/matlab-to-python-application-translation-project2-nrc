c660070aaf7b4290274199ea68e6c0ca88420408
Lucas Towers <luctowers@gmail.com>
2022-07-18 14:48:38 -0700

    Add specialized csv column reader and writer

--- nrcemt/alignment_software/engine/csv_io.py
+++ nrcemt/alignment_software/engine/csv_io.py
@@ -1,4 +1,6 @@
 import csv
+import enum
+from multiprocessing.sharedctypes import Value
 
 import numpy as np
 
@@ -15,3 +17,60 @@ def load_marker_csv(filename):
                 marker_position = (int(row[i*2]), int(row[i*2+1]))
                 marker_data[i].append(marker_position)
     return np.array(marker_data)
+
+
+def write_columns_csv(filename, columns):
+    with open(filename, 'a+', newline='') as csvfile:
+        csvfile.seek(0)
+        dictreader = csv.DictReader(csvfile)
+        fieldnames = dictreader.fieldnames
+        if fieldnames is None:
+            fieldnames = []
+        rows = list(dictreader)
+        for columnname, columndata in columns.items():
+            if columnname not in fieldnames:
+                fieldnames += [columnname]
+            for i, value in enumerate(columndata):
+                if value is None:
+                    value = ""
+                if i < len(rows):
+                    rows[i][columnname] = value
+                else:
+                    rows.append({columnname: value})
+            for i in range(len(columndata), len(rows)):
+                rows[i][columnname] = ""
+        csvfile.truncate(0)
+        csvfile.seek(0)
+        dictwriter = csv.DictWriter(csvfile, fieldnames)
+        dictwriter.writeheader()
+        while not any(rows[-1]):
+            rows.pop()
+        for row in rows:
+            dictwriter.writerow(row)
+
+
+def read_columns_csv(filename, columnnames):
+    with open(filename, 'r', newline='') as csvfile:
+        dictreader = csv.DictReader(csvfile)
+        fieldnames = dictreader.fieldnames = dictreader.fieldnames
+        intersection = list(set(fieldnames) & set(columnnames))
+        if len(intersection) != len(columnnames):
+            raise KeyError("not all columns are present in file")
+        columns = {}
+        for name in columnnames:
+            columns[name] = []
+        for row in dictreader:
+            for name in columnnames:
+                if name in row:
+                    value = row[name]
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        pass
+                    columns[name].append(value)
+                else:
+                    columns[name].append(None)
+        for name in columnnames:
+            while columns[name][-1] == "":
+                columns[name].pop()
+        return columns
--- nrcemt/alignment_software/test/test_csv_io.py
+++ nrcemt/alignment_software/test/test_csv_io.py
@@ -1,8 +1,10 @@
 import os
+from tempfile import TemporaryDirectory
 
 import numpy as np
+import pytest
 
-from nrcemt.alignment_software.engine.csv_io import load_marker_csv
+from nrcemt.alignment_software.engine.csv_io import load_marker_csv, read_columns_csv, write_columns_csv
 
 dirname = os.path.dirname(__file__)
 marker_filename = os.path.join(dirname, 'resources/marker_data.csv')
@@ -16,3 +18,40 @@ def test_load_marker_csv():
     np.testing.assert_equal(markers[0][1], [529, 850])
     np.testing.assert_equal(markers[0][2], [519, 850])
     np.testing.assert_equal(markers[1][0], [443, 726])
+
+
+def test_read_write_columns_csv():
+    with TemporaryDirectory() as tempdir:
+        with pytest.raises(FileNotFoundError):
+            read_columns_csv(os.path.join(tempdir, "foo.csv"), ["x"])
+        csvfilename = os.path.join(tempdir, "test.csv")
+        write_columns_csv(
+            csvfilename,
+            {
+                "x": [1, 2, 3],
+                "y": [4, 5, 6],
+                "z": [7, 8, 9, 10]
+            }
+        )
+        assert read_columns_csv(csvfilename, ["x"])["x"] == [1, 2, 3]
+        assert read_columns_csv(csvfilename, ["y"])["y"] == [4, 5, 6]
+        assert read_columns_csv(csvfilename, ["z"])["z"] == [7, 8, 9, 10]
+        assert read_columns_csv(csvfilename, ["y", "z"]) == {
+            "y": [4, 5, 6],
+            "z": [7, 8, 9, 10]
+        }
+        assert read_columns_csv(csvfilename, ["x", "z"]) == {
+            "x": [1, 2, 3],
+            "z": [7, 8, 9, 10]
+        }
+        write_columns_csv(
+            csvfilename,
+            {
+                "y": [42, "abc"]
+            }
+        )
+        assert read_columns_csv(csvfilename, ["x", "y", "z"]) == {
+            "x": [1, 2, 3],
+            "y": [42, "abc"],
+            "z": [7, 8, 9, 10]
+        }
