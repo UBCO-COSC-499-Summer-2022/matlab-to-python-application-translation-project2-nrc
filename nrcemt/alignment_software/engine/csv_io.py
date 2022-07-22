4372fca8eb24a0c8bd98e56f81c2326d8f06cdbd
Lucas Towers <luctowers@gmail.com>
2022-07-21 23:25:56 -0700

    Fix my broken fix

--- nrcemt/alignment_software/engine/csv_io.py
+++ nrcemt/alignment_software/engine/csv_io.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def load_marker_csv(filename):
-    marker_data = None
+    marker_data = []
     with open(filename, 'r', newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
-            if marker_data is None:
+            if marker_data == []:
                 marker_count = len(row) // 2
                 marker_data = [[] for i in range(marker_count)]
             for i in range(marker_count):
