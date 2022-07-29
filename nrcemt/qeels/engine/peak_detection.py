fixed rotation

nrcemt/qeels/engine/peak_detection.py
@@ -3,6 +3,11 @@ import numpy as np
 import scipy
 import scipy.signal
 import scipy.optimize
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from nrcemt.qeels.engine.spectrogram import (
+    process_spectrogram
+)
 
 SPEED_LIGHT = 3e8
 PLANCK_CONSTANT = 4.1357e-15
@@ -105,7 +110,7 @@ def find_peaks(spectrogram_ycfit):
 def rotate_spectrogram(spectrogram, rotation_angle_degrees):
     spectrogram_rotated = scipy.ndimage.rotate(
         spectrogram,
-        rotation_angle_degrees,
+        rotation_angle_degrees*-1,
         reshape=False
     )
 
@@ -268,8 +273,6 @@ def peak_detection(
     average_pixel = results_array[3]
     e_dispersion = results_array[0]
     images = []
-    # q_dispersion_upper = results_array[1]
-    # q_dispersion_lower = results_array[2]
 
     # loop through different rows
     for i in range(0, 6, 2):
@@ -326,6 +329,12 @@ def peak_detection(
             )
             images.append(peak_image)
             if i == 0:
+                # peak_position_y = loadmat("nrcemt\qeels\\test\\resources\\t (1).mat")
+                # peak_position_y = peak_position_y['Peak_position_y']
+
+                # peak_position_x = loadmat("nrcemt\qeels\\test\\resources\\t (2).mat")
+                # peak_position_x = peak_position_x['Peak_position_x']
+
                 e_dispersion, bulk_image = bulk_calculations(
                     peak_position_x, peak_position_y,
                     spectrogram
@@ -334,6 +343,10 @@ def peak_detection(
                 images.append(bulk_image)
 
             else:
+                # peak_position_y = loadmat("nrcemt\qeels\\test\\resources\\k (2).mat")
+                # peak_position_y = peak_position_y['Peak_position_y']
+                # peak_position_x = loadmat("nrcemt\qeels\\test\\resources\\k (1).mat")
+                # peak_position_x = peak_position_x['Peak_position_x']
                 dispersion_q, surface_image, _ = surface_plasmon_calculations(
                     peak_position_x, peak_position_y,
                     e_dispersion, spectrogram
@@ -346,9 +359,15 @@ def peak_detection(
                     results_array[2] = dispersion_q
 
     # need to combine the images better
-    result_image = (spectrogram)
+    result_image = np.zeros(spectrogram.shape)
+    results = (spectrogram)
 
     for image in images:
         result_image += image
 
-    return results_array, result_image
+    for a in range(spectrogram_width):
+        for b in range(spectrogram_height):
+            if result_image[a][b] != 0:
+                results[a][b] = 5000
+
+    return results_array, results

nrcemt/qeels/test/test_peak_detection.py
@@ -4,6 +4,7 @@ from nrcemt.qeels.engine.peak_detection import (
     calculation_q,
     compute_rect_corners,
     mark_peaks,
+    peak_detection,
     surface_plasmon_calculations,
     ycfit,
     calc_angle,
@@ -146,7 +147,7 @@ def test_mark_peaks():
 
     image3 = loadmat(image3_path)
     image3 = image3['image3']
-
+#[906, 258], [919, 696]
     # Subtracted 1 from value because value is
     # based off matlabs 1 based indexing
     peak_position_x, peak_position_y, image = mark_peaks(
@@ -335,3 +336,33 @@ def test_surface_plasmon_calculations():
     # np.testing.assert_allclose(
     #     image_result, image2
     # )
+
+
+def test_peak_detection():
+    dirname = os.path.dirname(__file__)
+    spectrogram_path = os.path.join(dirname, 'resources/Converted.prz')
+    spectrogram = load_spectrogram(spectrogram_path)
+
+    plasmon_array=[
+    [906, 258], [919, 696],
+    [677, 485], [789, 442],
+    [677, 524], [800, 573]]
+
+    width_array = [60, 60, 60]
+
+    results_array = [0.0569, 0.038, 0.038, 10]
+
+    detect_array = [1, 1, 1]
+
+    returned_results, returned_image = peak_detection(
+        plasmon_array, width_array,
+        results_array, detect_array,
+        spectrogram
+    )
+    # upper 0.46826
+    # lower 0.47733
+    # ev 0.051474
+
+    # WORKS IF USING MATLAB GENERATED PEAK ARRAYS !!!!!!!!!!!!!!!!!!!!!!!!!
+    # NOT  WITH OUR OWN PEAK ARRAYS!!!!!!
+    print(returned_results)