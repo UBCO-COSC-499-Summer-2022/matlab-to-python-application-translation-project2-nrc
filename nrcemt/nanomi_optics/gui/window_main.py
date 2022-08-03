9f7a6efc16fee037227b41d529df0177115453e0
josepena97 <penarevelo@gmail.com>
2022-08-03 09:48:21 -0700

    Refactoring window_main

--- nrcemt/nanomi_optics/gui/window_main.py
+++ nrcemt/nanomi_optics/gui/window_main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from .frame_above_sample import AboveSampleFrame
-from .frame_below_sample import BelowSampleFrame
+from .frame_lelow_sample import BelowSampleFrame
 from .frame_results import ResultsFrame
 from .frame_diagram import DiagramFrame
 from nrcemt.common.gui.async_handler import AsyncHandler
@@ -36,24 +36,24 @@ class MainWindow(tk.Tk):
         settings_frame.columnconfigure(1, weight=1)
 
         # Upper Settings
-        async_hand_c = AsyncHandler(self.update_cf_c)
+        async_hand_c = AsyncHandler(self.update_cf_u)
         self.upper_menu = AboveSampleFrame(settings_frame)
         self.upper_menu.grid(row=0, column=0, sticky="nwse")
         self.upper_menu.c1_link.set_command(async_hand_c)
-        self.upper_menu.c1_toggle.set_command(self.slider_status_c)
+        self.upper_menu.c1_toggle.set_command(self.slider_status_u)
         self.upper_menu.c2_link.set_command(async_hand_c)
-        self.upper_menu.c2_toggle.set_command(self.slider_status_c)
+        self.upper_menu.c2_toggle.set_command(self.slider_status_u)
         self.upper_menu.c3_link.set_command(async_hand_c)
-        self.upper_menu.c3_toggle.set_command(self.slider_status_c)
+        self.upper_menu.c3_toggle.set_command(self.slider_status_u)
 
         # Lower Settings
-        async_hand_b = AsyncHandler(self.update_cf_b)
+        async_hand_l = AsyncHandler(self.update_cf_l)
         self.lower_menu = BelowSampleFrame(settings_frame)
         self.lower_menu.grid(row=0, column=1, sticky="nwse")
-        self.lower_menu.distance_link.set_command(async_hand_b)
+        self.lower_menu.distance_link.set_command(async_hand_l)
         for i in range(len(self.lower_menu.sliders)):
-            self.lower_menu.links[i].set_command(async_hand_b)
-            self.lower_menu.buttons[i].set_command(self.slider_status_b)
+            self.lower_menu.links[i].set_command(async_hand_l)
+            self.lower_menu.buttons[i].set_command(self.slider_status_l)
         self.lower_menu.opt_sel.trace(
             "w", lambda a, b, c: self.optimization_mode()
         )
@@ -65,40 +65,40 @@ class MainWindow(tk.Tk):
         self.last_lens = -1
 
     # gets the values from all the slides and update lists
-    def update_cf_c(self, value):
-        self.diagram.cf_c = [
+    def update_cf_u(self, value):
+        self.diagram.cf_u = [
             float(self.upper_menu.c1_link.get()),
             float(self.upper_menu.c2_link.get()),
             float(self.upper_menu.c3_link.get())
         ]
-        self.diagram.update_c_lenses()
+        self.diagram.update_u_lenses()
 
     # turns slider on and off based on toggle status + name
-    def slider_status_c(self, value):
+    def slider_status_u(self, value):
         self.diagram.active_lc = [
             self.upper_menu.c1_toggle.get_status(),
             self.upper_menu.c2_toggle.get_status(),
             self.upper_menu.c3_toggle.get_status()
         ]
-        self.diagram.update_c_lenses()
+        self.diagram.update_u_lenses()
 
-    def update_cf_b(self, value):
+    def update_cf_l(self, value):
         self.diagram.distance_from_optical = \
             float(self.lower_menu.distance_link.get()) * (10**-6)
-        self.diagram.cf_b = [
+        self.diagram.cf_l = [
             float(i.get()) for i in self.lower_menu.links
         ]
 
-        self.diagram.update_b_lenses(
+        self.diagram.update_l_lenses(
             self.current_lens != -1, self.current_opt, self.current_lens
         )
         self.set_slider_opt()
 
-    def slider_status_b(self, value):
+    def slider_status_l(self, value):
         self.diagram.active_lb = [
             b.get_status() for b in self.lower_menu.buttons
         ]
-        self.diagram.update_b_lenses(
+        self.diagram.update_l_lenses(
             self.current_lens != -1, self.current_opt, self.current_lens
         )
         self.set_slider_opt()
@@ -111,7 +111,7 @@ class MainWindow(tk.Tk):
 
         if self.current_lens != -1:
             self.disable_lens_widgets(self.current_lens, True)
-            self.diagram.update_b_lenses(
+            self.diagram.update_l_lenses(
                 True, self.current_opt, self.current_lens
             )
             self.set_slider_opt()
@@ -119,7 +119,7 @@ class MainWindow(tk.Tk):
     def set_slider_opt(self):
         index = self.current_lens
         if index != -1:
-            self.lower_menu.links[index].set(self.diagram.cf_b[index])
+            self.lower_menu.links[index].set(self.diagram.cf_l[index])
 
     def disable_lens_widgets(self, index, opt):
         if index != -1:
