7c8bba82df203a59e6f9f3df6ec327f9a7e6a4c9
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-08-04 16:40:09 -0600

    specified file type

--- nrcemt/qeels/gui/main_window.py
+++ nrcemt/qeels/gui/main_window.py
@@ -216,7 +216,9 @@ class MainWindow(tk.Tk):
 
     def open_image(self):
         # Potentially add ability to filter by file types
-        file_path = tk.filedialog.askopenfilename()
+        file_path = tk.filedialog.askopenfilename(
+            filetypes = [("Prz File", "*.prz")]
+        )
         if len(file_path) != 0:
             # Rendering spectrogram
             # If error loading file, error message is displayed
