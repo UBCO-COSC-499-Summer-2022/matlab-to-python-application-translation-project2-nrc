6edc98637702a9cedcab1cd2ea3d4c633489ad39
josepena97 <penarevelo@gmail.com>
2022-08-04 02:35:13 -0700

    Lense excitation functions

--- nrcemt/nanomi_optics/engine/lens_excitation.py
+++ nrcemt/nanomi_optics/engine/lens_excitation.py
@@ -2,5 +2,13 @@ def ur_symmetric(focal_l):
     return ((focal_l + 3.723) / 9.709) ** (-1 / 2.503)
 
 
-def ur_asymetric(focal_l):
+def ur_asymmetric(focal_l):
     return ((focal_l + 0.811) / 7.59) ** (-1 / 2.727)
+
+
+def cf_symmetric(ur):
+    return ((ur ** (2.503 / -1)) * 9.709) - 3.723
+
+
+def cf_asymmetric(ur):
+    return ((ur ** (2.727 / -1)) * 7.59) - 0.811
