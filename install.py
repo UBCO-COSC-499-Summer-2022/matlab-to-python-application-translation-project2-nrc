aa4c53d587180ba895eabbf7bc6f07e59e905002
Lucas Towers <luctowers@gmail.com>
2022-08-09 13:45:16 -0700

    Detect the location of python install script rather than using working directory

--- install.py
+++ install.py
@@ -1,9 +1,12 @@
 import subprocess
 import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def install():
     # use pip to install the nrcemt package at its current location
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "-e", "."])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "-e", dir_path])
 
 if __name__ == "__main__":
     install()
