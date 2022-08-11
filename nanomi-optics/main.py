b307bbde7d366d322df6b7710b26d80c40956b58
Lucas Towers <luctowers@gmail.com>
2022-08-10 23:18:21 -0700

    Add project structure for nanomi

---
+++ nanomi-optics/main.py
@@ -0,0 +1,4 @@
+import nanomi_optics
+
+if __name__ == "__main__":
+    nanomi_optics.main()
---
+++ nanomi-optics/nanomi_optics/__init__.py
@@ -0,0 +1,6 @@
+from .gui.window_main import MainWindow
+
+
+def main():
+    main_window = MainWindow()
+    main_window.mainloop()
