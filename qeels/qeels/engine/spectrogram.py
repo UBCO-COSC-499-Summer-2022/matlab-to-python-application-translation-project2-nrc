7874ce19a3803e77125482698a3c5125907df941
veengren-s <71776777+veengren-s@users.noreply.github.com>
2022-08-11 20:56:16 -0600

    rough draft of docstrings

--- qeels/qeels/engine/spectrogram.py
+++ qeels/qeels/engine/spectrogram.py
@@ -2,10 +2,16 @@ import numpy as np
 
 
 def load_spectrogram(file_path):
+    """
+    Loads the data from the prz file from provied file_path
+    """
     spectrogram = np.load(file_path, allow_pickle=True)
     return spectrogram['data']
 
 
 def process_spectrogram(spectrogram):
+    """
+    Performs basie processing on provided spectrogram
+    """
     spectrogram = np.log(np.absolute(spectrogram+1))
     return spectrogram
