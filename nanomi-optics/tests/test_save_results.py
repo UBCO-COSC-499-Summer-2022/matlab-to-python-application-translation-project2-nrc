3d3ba8bdb95fcd26a441bfa48f34d67d85e91e2b
Lucas Towers <luctowers@gmail.com>
2022-08-16 14:00:26 -0700

    Use a tempdir instead of having relative output for test case

--- nanomi-optics/tests/test_save_results.py
+++ nanomi-optics/tests/test_save_results.py
@@ -1,10 +1,10 @@
 import os
 import csv
+from tempfile import TemporaryDirectory
 from nanomi_optics.engine.save_results import save_csv
 
 dirname = os.path.dirname(__file__)
 test_filename = os.path.join(dirname, 'resources/test.csv')
-output_filename = os.path.join(dirname, 'resources/output.csv')
 
 
 def test_csv():
@@ -13,12 +13,6 @@ def test_csv():
     col_3 = [7, 8, 9]
     col_act = [True, False, True]
 
-    save_csv(
-            col_1, col_act, col_2,
-            col_1, col_act,
-            col_3, col_2, 1,
-            2, output_filename
-        )
     test = []
     with open(test_filename, 'r') as csvfile:
         csv_test = csv.reader(csvfile)
@@ -26,9 +20,17 @@ def test_csv():
             test.append(row)
 
     output = []
-    with open(output_filename, 'r') as csvfile:
-        csv_result = csv.reader(csvfile)
-        for row in csv_result:
-            output.append(row)
+    with TemporaryDirectory() as tempdir:
+        output_filename = os.path.join(tempdir, "output.csv")
+        save_csv(
+            col_1, col_act, col_2,
+            col_1, col_act,
+            col_3, col_2, 1,
+            2, output_filename
+        )
+        with open(output_filename, 'r') as csvfile:
+            csv_result = csv.reader(csvfile)
+            for row in csv_result:
+                output.append(row)
 
     assert test == output
